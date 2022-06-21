a80beb139de243d58e2ece20b5a307eba349d07a
rorosana <107406381+rorosana@users.noreply.github.com>
2022-06-21 13:52:53 +0200

    Updated  2nd line on e1 from techTeam1 on GitHub

--- e1-forloop.py
+++ e1-forloop.py
@@ -40,3 +40,5 @@ print("First modification for the 2nd commit")
 print("Added this line of code from techdevelopers from git terminal")
 #------------------------------------------------------
 print("Added 1st line from techTeam1 on GitHub")
+#------------------------------------------------------
+print("Added 2nd line from techTeam1 on GitHub")
